59c71438688bb72a9a2ae43d7b6b9323e2cdbee8
Chris DeCleene <chris_decleene@ymail.com>
2021-08-31 12:48:04 -0600

    Remove commented out formulas, including player input labels, and add 14 and 16 team options.

--- main.js
+++ main.js
@@ -3,38 +3,58 @@
  Create Page On Load
 ****************************************************************/
 function pickLeagueSize() {
+  // Page Title
   const leagueSizeHeader = document.createElement('h2')
   leagueSizeHeader.textContent = 'Select League Size';
 
+  // Create label for dropdown menu
   const selectTeamsTitle = document.createElement('label');
   selectTeamsTitle.textContent = 'Select Number Of Teams';
   selectTeamsTitle.for = 'teams';
 
+  // Create select element for dropdown menu
   const selectTeams = document.createElement('select');
   selectTeams.id = 'teams';
   selectTeams.name = 'teams';
 
+  // 8 Team Option
   const option1 = document.createElement('option');
   option1.value = 8;
   option1.textContent = '8';
 
+  // 10 Team Option
   const option2 = document.createElement('option');
   option2.value = 10;
   option2.textContent = '10';
 
+  // 12 Team Option
   const option3 = document.createElement('option');
   option3.value = 12;
   option3.textContent = '12';
 
+  // 14 Team Option
+  const option4 = document.createElement('option');
+  option4.value = 14;
+  option4.textContent = '14';
+
+  // 16 Team Option
+  const option5 = document.createElement('option');
+  option5.value = 16;
+  option5.textContent = '16';
+
+  // Add all options to select element parent
   selectTeams.appendChild(option1);
   selectTeams.appendChild(option2);
   selectTeams.appendChild(option3);
+  selectTeams.appendChild(option4);
+  selectTeams.appendChild(option5);
 
+  // Create button for submission
   const submitTeams = document.createElement('input');
   submitTeams.type = 'submit';
   submitTeams.id = 'submit-teams'
 
-  // Create element to store number of teams element
+  // Create parent element to store all other elements
   const teamsDiv = document.createElement('div');
   teamsDiv.id = 'teams-div';
   teamsDiv.appendChild(leagueSizeHeader);
@@ -42,10 +62,11 @@ function pickLeagueSize() {
   teamsDiv.appendChild(selectTeams);
   teamsDiv.appendChild(submitTeams);
 
-  // Create Team Picker
+  // Push parent element to the DOM
   document.getElementById('app').appendChild(teamsDiv);
 }
 
+// Triggers first form upon page load
 window.addEventListener('load', () => {
   pickLeagueSize()
 }, false)
@@ -57,34 +78,35 @@ Create Name Input Section On Submit
 
 // Create inputs for each team in the league after commissioner submits the number of teams
 function createInput (teams) {
-  const nameDiv = document.createElement('div');
+
+  // Create parent Div to hold all inputs and button
+  const nameDiv = document.createElement('div');  
+  nameDiv.id = 'name-div'
+
+  // Create and add title to parent div
   const nameTitle = document.createElement('h2');
   nameTitle.textContent = 'List Player Names';
   nameDiv.appendChild(nameTitle);
-  nameDiv.id = 'name-div'
+
   for(let i = 1; i <= teams; i++) {
-    /*
-    const nameLabel = document.createElement('label');
-    nameLabel.for = 'player' + i;
-    nameLabel.textContent = `Player ${i} Name:`
-    */
+    // Create an input for each player in league
     const nameInput = document.createElement('input');
     nameInput.type = 'text';
     nameInput.id = 'player' + i;
     nameInput.className = 'player';
-    nameInput.placeholder = 'Player Name'
-    // nameDiv.appendChild(nameLabel);
-    nameDiv.appendChild(nameInput)
-    document.getElementById('app').appendChild(nameDiv);
+    nameInput.placeholder = `Player ${i} Name`;
+
+    // Add inputs to parent Div
+    nameDiv.appendChild(nameInput);
   }
-  // Create submit for team names
+  // Create submit button for team names
   const submitNames = document.createElement('input');
   submitNames.type = 'submit';
   submitNames.id = 'submit-names';
   submitNames.className = 'button';
   nameDiv.appendChild(submitNames);
 
-  // Add to DOM
+  // Add entire Div to DOM
   document.getElementById('app').appendChild(nameDiv);
 
   // Remove number selector
@@ -92,6 +114,7 @@ function createInput (teams) {
   document.getElementById('app').removeChild(teamsDiv)
 };
 
+// Trigger next form upon submission of league size
 document.addEventListener('click',function(e){
   if(e.target && e.target.id== 'submit-teams'){
     const teams = document.getElementById('teams').value;
